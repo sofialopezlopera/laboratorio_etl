26a3c45437c08fc372aa85ded7de9750790303d3
f-villa4 <f.villa@udea.edu.co>
2026-06-07 17:26:04 -0500

    feat: implementar transformacion base con pandas

--- app/services/etl_service.py
+++ app/services/etl_service.py
@@ -1,7 +1,14 @@
 from typing import Any, Dict, List
 
+import pandas as pd
+
 from app.database import get_mongo_collection
 
 def obtener_productos_desde_mongo() -> List[Dict[str, Any]]:
     collection = get_mongo_collection()
     return list(collection.find({}).sort("_id", 1))
+
+def transformar_productos(documentos: List[Dict[str, Any]]) -> pd.DataFrame:
+    df = pd.json_normalize(documentos)
+    df_sql = pd.DataFrame()
+    return df_sql
